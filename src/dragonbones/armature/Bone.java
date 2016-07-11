d67fec82d070d0d7a006605b8b467d19cb54c5b7
mebius <mcewei@qq.com>
2016-07-11 17:46:55 +0800

    add armature package

---
+++ src/dragonbones/armature/Bone.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dragonbones.armature;
+
+/**
+ *
+ * @author mebius
+ */
+public class Bone {
+    
+}
